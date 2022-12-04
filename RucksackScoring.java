99d5f440a1ef452ae7a8d09a3caff0328ab94f05
Will Roddy <william.t.roddy@gmail.com>
2022-12-04 16:51:13 -0700

    adding part two solution

--- RucksackScoring.java
+++ RucksackScoring.java
@@ -25,21 +25,33 @@ public class RucksackScoring {
         return score;
     }
 
-    static String sharedCharacter(String compartment_one_string , String  compartment_two_string){
+    static String sharedCharacter(String set_one_string , String  set_two_string, String set_three_string){
         // https://stackoverflow.com/a/30662665
         Set<Character> set1 = new HashSet<Character>();
         Set<Character> set2 = new HashSet<Character>();
 
-        for(char c : compartment_one_string.toCharArray()){
+        for(char c : set_one_string.toCharArray()){
             set1.add(c);
         }
 
-        for(char c : compartment_two_string.toCharArray()){
+        for(char c : set_two_string.toCharArray()){
             set2.add(c);
         }
 
         // get the intersection of the two sets 
         set1.retainAll(set2);
+
+        // conditionally process a third string set 
+        if (!set_three_string.isEmpty()) {
+            Set<Character> set3 = new HashSet<Character>();
+            for(char c : set_three_string.toCharArray()){
+                set3.add(c);
+            }
+
+            // get the intersection
+            set1.retainAll(set3);
+    
+        }
 
         // convert to a string
         String shared_string = new String();
@@ -59,6 +71,7 @@ public class RucksackScoring {
         ArrayList<String> group_rucksack = new ArrayList<String>();
         int group_counter = 0;
         int group_size = 3;
+        int group_priority_sum = 0;
 
         // Read the input and loop through each line 
         for (String line : Files.readAllLines(Paths.get(input_data_path))) {
@@ -74,33 +87,36 @@ public class RucksackScoring {
 
             // if the group is complete, print the groups rucksacks and reset 
             if(group_counter==group_size){
-                System.out.println(group_rucksack);
+                
+                // get the shared character for the group
+                String shared_group_character = sharedCharacter(group_rucksack.get(0).toString(), group_rucksack.get(1).toString(), group_rucksack.get(2).toString());
+                char group_letter = shared_group_character.charAt(0);
+                int group_score = convertLetterToScore(group_letter);
+                group_priority_sum = group_priority_sum+group_score;
+
+                // update count and reset array 
                 group_counter=0;
                 group_rucksack.clear();
             }
 
             // split the string into compartments
             int rucksack_length = rucksack.length();
-            String compartment_one_string = rucksack.substring(0, rucksack_length/2);
-            String compartment_two_string = rucksack.substring(rucksack_length/2);
-
-            // System.out.println("Split string: "+compartment_one_string+"    "+compartment_two_string);
-            
+            String set_one_string = rucksack.substring(0, rucksack_length/2);
+            String set_two_string = rucksack.substring(rucksack_length/2);            
             // Find the Letter shared in both compartments
-            String shared_character = sharedCharacter(compartment_one_string, compartment_two_string);
+            String shared_character = sharedCharacter(set_one_string, set_two_string, "");
             char my_letter = shared_character.charAt(0);
 
-            // Convert to the score 
+            // Convert to the score and add to priority score 
             int score = convertLetterToScore(my_letter);
-            // System.out.println("Character "+my_letter+" has score of: "+score);
-
-            // add to the priority sum 
             priority_sum = priority_sum+score;
 
 
         }
 
-        System.out.println("Priority Sum: "+priority_sum);
+        // print the results 
+        System.out.println("(Part 1) Individaul Priority Sum: "+priority_sum);
+        System.out.println("(Part 2) Group Priority Sum: "+group_priority_sum);
 
     }
     
